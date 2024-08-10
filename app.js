bf0d5964d527e4d755fddd7de1dd91a3855f517e
shrex01 <shreyasu123@gmail.com>
2024-08-10 17:12:54 +0530

    Episode 3 commit

--- app.js
+++ app.js
@@ -16,19 +16,19 @@ const Heading3 =()=>{  //REACT FUNCTIONAL COMPONENT
 }
 
 //ACHIEVING NESTING INSIDE REACT FUNCTIONAL COMPONENTS
-const Title =()=>{
+const Title =()=>{                             //REACT COMPONENT (functional)
     return( <h1>TITLE</h1>);
 }
 
-const Element = <h1>react element rendered</h1>
+const Element = <h1>react element rendered</h1> //REACT ELEMENT 
 
 const Body1 = () =>{
     //COMPONENT COMPOSITION: 
     //(Components composing of eachother || components inside one another || components containing other components)
     return(
     <div>
-        {Element} {/*Rendering react elements*/}
-        
+        {Element} {/*Rendering react element*/}
+
         {/* the below 3 all mean the same */}
             {Title()}
             <Title/>
