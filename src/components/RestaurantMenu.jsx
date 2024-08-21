7dfdcf66427712433f19c2bfbcb20f7c557e08ea
shrex01 <shreyasu123@gmail.com>
2024-08-21 22:21:44 +0530

    SWIGGY API issue fixed

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,14 +1,29 @@
-const RestaurantMenu =()=>{
-  return(
-    <div className="menu">
-      <h2>Name of Restaurant</h2>
-      <h2>Menu</h2>
-      <ul>
-        <li>Biryani</li>
-        <li>Roti</li>
-        <li>Curry</li>
-      </ul>
-    </div>
-  )
+import { useEffect, useState } from "react"
+import { Shimmer } from "./shimmer";
+
+const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState();
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=65797&catalog_qa=undefined&submitAction=ENTER"
+    );
+    const response = await data.json();
+    setResInfo(response.data);
+  }
+  console.log('resInfo:', resInfo)
+
+  const { name, cusines, costForTwoMessage } = resInfo?.cards[2]?.card?.card?.info||{};
+  return (resInfo == null) ? (<Shimmer />) :
+    (
+      <div className="menu">
+        <h2>{name}</h2>
+        <h2>Menu</h2>
+        <h3>{cusines?.join(",")}</h3>
+        <h3>{costForTwoMessage}</h3>
+      </div>
+    )
 }
 export default RestaurantMenu
